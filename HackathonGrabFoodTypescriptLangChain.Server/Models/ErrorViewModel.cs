dda5abd49c4eff1d5e9c744416c060c6e5ba32dd
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-07-27 14:12:20 +0200

    init

---
+++ HackathonGrabFoodTypescriptLangChain.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HackathonGrabFoodTypescriptLangChain.Server.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
